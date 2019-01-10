d8469a7c35f7ea15c963b66fb3de537875be076b
Juan Moreno <jmoreno.0312@gmail.com>
2019-01-10 00:15:04 -0800

    Initial implementation complete.

--- MusicalLightsII/lib/FrequencyMeter/FrequencyMeter.cpp
+++ MusicalLightsII/lib/FrequencyMeter/FrequencyMeter.cpp
@@ -1,10 +1,11 @@
 #include <FrequencyMeter.h>
 
 FrequencyMeter::FrequencyMeter() {
-    ;
+    // number of triggers starts off as zero
+    this.number_of_triggers = 0;
 }
 
-void FrequencyMeter::Initialize(unsigned int prescaler_value) {
+void FrequencyMeter::Initialize(uint8_t prescaler_value) {
   cli(); //disable interrupts
   //set up continuous sampling of analog pin 0 at 38.5kHz:
   //clear ADCSRA and ADCSRB registers
@@ -20,12 +21,16 @@ void FrequencyMeter::Initialize(unsigned int prescaler_value) {
   sei();//enable interrupts
 }
 
-void FrequencyMeter::AddFrequencyListener(int pin_trigger, int frequency, int threshold) {
-    // TODO: Write frequency and threshold to private fields
+void FrequencyMeter::AddFrequencyListener(uint8_t pin_trigger, uint16_t frequency, uint16_t threshold) {
+    this.triggers[this.number_of_triggers] = pin_trigger;
+    this.frequencies[this.number_of_triggers] = frequency;
+    this.thresholds[this.number_of_triggers] = threshold;
     pinMode(pin_trigger, OUTPUT);
+    // increase number of triggers
+    this.number_of_triggers++;
 }
 
-int FrequencyMeter::GetBinIndexFromFrequency(int frequency) {
+int FrequencyMeter::GetBinIndexFromFrequency(uint16_t frequency) {
   int chunk_size = SAMPLE_RATE/(FHT_N/2);
   int bin_index = frequency/chunk_size;
   
@@ -33,14 +38,14 @@ int FrequencyMeter::GetBinIndexFromFrequency(int frequency) {
 }
 
 void FrequencyMeter::DisplayLevels() {
-  int pin = 13;
   int value;
-  for(uint16_t i = 0; i < (sizeof(frequencies)/sizeof(int)); i++) {
-    value = fht_log_out[GetBinIndexFromFrequency(frequencies[i])];
-    if (value > freq_thresholds[i]) {
-      digitalWrite(pin, HIGH);
-    } else { digitalWrite(pin, LOW); }
-    pin--;
+  for(uint16_t i = 0; i < this.number_of_triggers; i++) {
+    value = fht_log_out[GetBinIndexFromFrequency(this.frequencies[i])];
+    if (value > this.thresholds[i]) {
+        digitalWrite(this.triggers[i], HIGH);
+    } else { 
+        digitalWrite(this.triggers[i], LOW); 
+    }
   }
 }
 
--- MusicalLightsII/lib/FrequencyMeter/FrequencyMeter.h
+++ MusicalLightsII/lib/FrequencyMeter/FrequencyMeter.h
@@ -1,15 +1,19 @@
+#define BUFFER_SIZE 10
+#include <Arduino.h>
+
 class FrequencyMeter {
     private:
-        int *triggers;
-        int *frequencies;
-        int *thresholds;
-        int prescaler_value;
-        int adc_sample_rate;
-        int GetBinIndexFromFrequency(int frequency);
+        uint8_t number_of_triggers;
+        uint16_t triggers[BUFFER_SIZE];
+        uint16_t frequencies[BUFFER_SIZE];
+        uint16_t thresholds[BUFFER_SIZE];
+        uint8_t prescaler_value;
+        uint16_t adc_sample_rate;
+        uint16_t GetBinIndexFromFrequency(uint16_t frequency);
     public:
         FrequencyMeter();
-        void Initialize(unsigned int prescaler_value);
+        void Initialize(uint8_t prescaler_value);
         void DisplayLevels();
         void ReadFrequencies();
-        void AddFrequencyListener(int pin_trigger, int frequency, int threshold);
+        void AddFrequencyListener(uint8_t pin_trigger, uint16_t frequency, uint16_t threshold);
 }
