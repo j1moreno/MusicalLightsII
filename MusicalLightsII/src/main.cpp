f6fb2c833e12110878c3bc5aa271cb960f98de32
Juan Moreno <jmoreno.0312@gmail.com>
2019-01-10 01:40:54 -0800

    Slight tweak to frequencies to get better response from lights.

--- MusicalLightsII/src/main.cpp
+++ MusicalLightsII/src/main.cpp
@@ -9,12 +9,12 @@ FrequencyMeter meter;
 
 void setup(){
   meter.Initialize(PRESCALER_VALUE, SAMPLE_RATE);
-  meter.AddFrequencyListener(13, 40, 175);
+  meter.AddFrequencyListener(13, 75, 175);
   meter.AddFrequencyListener(12, 200, 154);
   meter.AddFrequencyListener(11, 500, 120);
   meter.AddFrequencyListener(10, 40, 140);
   meter.AddFrequencyListener(9, 5000, 75);
-  meter.AddFrequencyListener(8, 10000, 75);
+  meter.AddFrequencyListener(8, 9000, 75);
 }
 
 void loop() {
