682a6178f3859e3e5bf043206a929dbd9a3f565d
Juan Moreno <jmoreno.0312@gmail.com>
2019-02-19 01:07:34 -0800

    Fixed tabs in header file, added define guard.

--- MusicalLightsII/lib/FrequencyMeter/FrequencyMeter.h
+++ MusicalLightsII/lib/FrequencyMeter/FrequencyMeter.h
@@ -1,20 +1,25 @@
+#ifndef FREQUENCY_METER_H_
+#define FREQUENCY_METER_H_
+
 #define BUFFER_SIZE 10
 #include <Arduino.h>
 
 class FrequencyMeter {
-    private:
-        uint8_t number_of_triggers;
-        uint16_t triggers[BUFFER_SIZE];
-        uint16_t frequencies[BUFFER_SIZE];
-        uint16_t thresholds[BUFFER_SIZE];
-        uint8_t prescaler_value;
-        uint16_t adc_sample_rate;
-        uint16_t GetBinIndexFromFrequency(uint16_t frequency);
-        uint8_t TranslatePrescaleValue(uint8_t prescaler_value);
-    public:
-        FrequencyMeter();
-        void Initialize(uint8_t prescaler_value, uint16_t sample_rate);
-        void DisplayLevels();
-        void ReadFrequencies();
-        void AddFrequencyListener(uint8_t pin_trigger, uint16_t frequency, uint16_t threshold);
+ public:
+  FrequencyMeter();
+  void Initialize(uint8_t prescaler_value, uint16_t sample_rate);
+  void DisplayLevels();
+  void ReadFrequencies();
+  void AddFrequencyListener(uint8_t pin_trigger, uint16_t frequency, uint16_t threshold);
+
+ private:
+  uint8_t number_of_triggers;
+  uint16_t triggers[BUFFER_SIZE];
+  uint16_t frequencies[BUFFER_SIZE];
+  uint16_t thresholds[BUFFER_SIZE];
+  uint8_t prescaler_value;
+  uint16_t adc_sample_rate;
+  uint16_t GetBinIndexFromFrequency(uint16_t frequency);
+  uint8_t TranslatePrescaleValue(uint8_t prescaler_value);
 };
+#endif  // FREQUENCY_METER_H_
