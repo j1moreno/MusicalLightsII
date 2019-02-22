693cba698d3faeaea51fae6cea92a91eee5de595
Juan Moreno <jmoreno.0312@gmail.com>
2019-02-22 03:26:51 -0800

    Tweaked frequencies

--- MusicalLightsII/src/main.cpp
+++ MusicalLightsII/src/main.cpp
@@ -1,20 +1,18 @@
 #include <Arduino.h>
 #include <FrequencyMeter.h>
-// sampling rate = adc default freq * default prescaler / custom prescaler
-// 38.4kHz = 9.6kHz * 128 / 32
-#define SAMPLE_RATE 19200 
-#define PRESCALER_VALUE 32
+
+#define SAMPLE_RATE 40000 
 
 FrequencyMeter meter;
 
 void setup(){
   meter.Initialize(A0, SAMPLE_RATE);
   meter.AddFrequencyListener(13, 100, 154);
-  meter.AddFrequencyListener(12, 200, 154);
-  meter.AddFrequencyListener(11, 500, 120);
+  meter.AddFrequencyListener(12, 400, 154);
+  meter.AddFrequencyListener(11, 1000, 120);
   meter.AddFrequencyListener(10, 40, 140);
-  meter.AddFrequencyListener(9, 5000, 75);
-  meter.AddFrequencyListener(8, 9000, 75);
+  meter.AddFrequencyListener(9, 10000, 75);
+  meter.AddFrequencyListener(8, 16000, 75);
 }
 
 void loop() {
